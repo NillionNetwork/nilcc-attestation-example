f93547d18a338cf1ce6d372da2c3bb0d4af125a4
crypblizz <tom.terado@nillion.com>
2025-11-18 13:32:37 +0100

    chore: rm backslash

--- scripts/update-verification.sh
+++ scripts/update-verification.sh
@@ -19,7 +19,7 @@ fi
 VCPUS=${VCPUS:-1}
 NILCC_VERSION=${NILCC_VERSION:-0.2.1}
 MANIFEST_KEY=${MANIFEST_KEY:-$NILCC_VERSION}
-ALLOWED_DOMAINS='["nilcc.nillion.com", "github.com", "nilcc-attestation-example.vercel.app/"]'
+ALLOWED_DOMAINS='["nilcc.nillion.com", "github.com", "nilcc-attestation-example.vercel.app"]'
 
 MEASUREMENT_HASH=$(
   docker run -v/tmp/nilcc-verifier-cache:/tmp/nilcc-verifier-cache --rm \
