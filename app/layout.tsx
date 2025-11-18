7f218ca5345d0f0222018b3ded95ff9a45ecde5e
crypblizz <tom.terado@nillion.com>
2025-11-18 13:05:17 +0100

    chore: nextjs setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: 'Nilcc Attestation Example',
+  description: 'Nillion Network attestation example',
+}
+
 export default function RootLayout({
     children,
   }: {
