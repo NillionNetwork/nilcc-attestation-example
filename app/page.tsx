92fd3e2b79749c5ff9d7065bf89d25f7e2779e4f
crypblizz <tom.terado@nillion.com>
2025-11-18 14:01:40 +0100

    chore: new iFrame demo 3

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <h1> hello demo </h1>
+        <h1> hello demo 3 </h1>
         <iframe
           src="https://nilcc.nillion.com/api/badge?verificationUrl=https%3A%2F%2Fgithub.com%2FNillionNetwork%2Fnilcc-attestation-example%2Fblob%2Fmain%2Fverification-manifest.json&reportUrl=https%3A%2F%2F74850b5b-33e2-493b-982e-43aaad9df1bb.workloads.nilcc.sandbox.nillion.network%2Fnilcc%2Fapi%2Fv2%2Freport"
   width={260}
