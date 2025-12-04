a50e309bca60b1f7cbbc0dcac07d5e0bffa64e35
crypblizz <tom.terado@nillion.com>
2025-12-04 16:11:37 +0000

    chore: cache expiry

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,14 @@ export default function Home() {
       <div>
         <h1 className="mb-2"> Badge Demo (Correct)</h1>
         <iframe
-          src="http://nilcc.nillion.com/api/badge?verificationUrl=https%3A%2F%2Fgithub.com%2FNillionNetwork%2Fnilcc-attestation-example%2Fblob%2Fmain%2Fverification-manifest.json&reportUrl=https%3A%2F%2F74850b5b-33e2-493b-982e-43aaad9df1bb.workloads.nilcc.sandbox.nillion.network%2Fnilcc%2Fapi%2Fv2%2Freport"
+          src="http://nilcc.nillion.com/api/badge?verificationUrl=https%3A%2F%2Fgithub.com%2FNillionNetwork%2Fnilcc-attestation-example%2Fblob%2Fmain%2Fverification-manifest.json&reportUrl=https%3A%2F%2F36cd5019-e53b-4788-9a76-4775343fedeb.workloads.nilcc.sandbox.nillion.network%2Fnilcc%2Fapi%2Fv2%2Freport"
           width={260}
           height={90}
           style={{ border: 'none' }}
         />
         <h1 className="mb-2"> Badge Demo (Incorrect)</h1>
         <iframe
-          src="http://localhost:3000/api/badge?verificationUrl=https%3A%2F%2Fgithub.com%2FNillionNetwork%2Fnilcc-attestation-example%2Fblob%2Fmain%2Fverification-manifest.json&reportUrl=https%3A%2F%2F74850b5b-33e2-493b-982e-43aaad9df1bb.workloads.nilcc.sandbox.nillion.network%2Fnilcc%2Fapi%2Fv2%2Freport"
+          src="http://localhost:3001/api/badge?verificationUrl=https%3A%2F%2Fgithub.com%2FNillionNetwork%2Fnilcc-attestation-example%2Fblob%2Fmain%2Fverification-manifest.json&reportUrl=https%3A%2F%2F36cd5019-e53b-4788-9a76-4775343fedeb.workloads.nilcc.sandbox.nillion.network%2Fnilcc%2Fapi%2Fv2%2Freport"
           width={260}
           height={90}
           style={{ border: 'none' }}
