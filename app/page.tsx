b79b90bd6d84a39858631fac116a29c25277d81e
crypblizz <tom.terado@nillion.com>
2025-12-04 16:15:42 +0000

    chore: missing s

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div>
         <h1 className="mb-2"> Badge Demo (Correct)</h1>
         <iframe
-          src="http://nilcc.nillion.com/api/badge?verificationUrl=https%3A%2F%2Fgithub.com%2FNillionNetwork%2Fnilcc-attestation-example%2Fblob%2Fmain%2Fverification-manifest.json&reportUrl=https%3A%2F%2F36cd5019-e53b-4788-9a76-4775343fedeb.workloads.nilcc.sandbox.nillion.network%2Fnilcc%2Fapi%2Fv2%2Freport"
+          src="https://nilcc.nillion.com/api/badge?verificationUrl=https%3A%2F%2Fgithub.com%2FNillionNetwork%2Fnilcc-attestation-example%2Fblob%2Fmain%2Fverification-manifest.json&reportUrl=https%3A%2F%2F36cd5019-e53b-4788-9a76-4775343fedeb.workloads.nilcc.sandbox.nillion.network%2Fnilcc%2Fapi%2Fv2%2Freport"
           width={260}
           height={90}
           style={{ border: 'none' }}
